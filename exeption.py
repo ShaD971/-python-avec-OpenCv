1b466333626a4e502aeb83b42218ecc59cd20fae
kevin <kevin.toleon@hotmail.fr>
2018-11-25 20:00:24 +0100

    commit

--- exeption.py
+++ exeption.py
@@ -1,8 +1,4 @@
 try:
-    resultat = numerateur / denominateur
-except NameError:
-    print("La variable numerateur ou denominateur n'a pas été définie.")
-except TypeError:
-    print("La variable numerateur ou denominateur possède un type incompatible avec la division.")
-except ZeroDivisionError:
-    print("La variable denominateur est égale à 0.")
+    # Bloc de test
+except type_de_l_exception as exception_retournee:
+    print("Voici l'erreur :", exception_retournee)
