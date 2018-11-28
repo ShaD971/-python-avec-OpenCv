1fa52cd559dfb80867c27219a283f6714fd95e82
kevin <kevin.toleon@hotmail.fr>
2018-11-28 18:56:55 +0100

    commit

--- projet/detection_Form.py
+++ projet/detection_Form.py
@@ -1,13 +1,17 @@
 import cv2
+import numpy as np
 
-#on va charger l'image
-image = cv2.imread("pokemon.png")
-edged = cv2.Canny(image, 30, 150)
-while (1):
-#afficher l'image
- cv2.imshow("Image", image)
- cv2.imshow("Edged", edged)
- if cv2.waitKey(1) & 0xff == ord('q'):
-    break
+img_rgb = cv2.imread('pokemon.png')
+img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
-cv2.destroyAllWindows()
+template = cv2.imread('pokemon.png',0)
+w, h = template.shape[::-1]
+
+res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+threshold = 0.8
+loc = np.where( res >= threshold)
+
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+
+cv2.imshow('Detected',img_rgb)
--- projet/projet.py
+++ projet/projet.py
@@ -1,31 +1,81 @@
-import cv2
+import cv2   
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+#capture webcam
+cap=cv2.VideoCapture(0)
 
 while(1):
+    _, img = cap.read()
+        
+    #convertie frame(img  BGR) en HSV (hue-saturation-value)
+    hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
-    # Take each frame
-    _, frame = cap.read()
+    #definir la gamme de couleur rouge
+    red_lower=np.array([136,87,111],np.uint8)
+    red_upper=np.array([180,255,255],np.uint8)
 
-    # Convert BGR to HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    #définir la gamme de couleur bleu
+    blue_lower=np.array([99,115,150],np.uint8)
+    blue_upper=np.array([110,255,255],np.uint8)
+    
+    #definie la game de couleur jaune
+    yellow_lower=np.array([22,60,200],np.uint8)
+    yellow_upper=np.array([60,255,255],np.uint8)
 
-    # define range of blue color in HSV
-    lower_blue = np.array([110,50,50])
-    upper_blue = np.array([130,255,255])
+    #rouver la gamme de couleurs rouge, bleue et jaune dans l'image
+    rouge=cv2.inRange(hsv, red_lower, red_upper)
+    bleu=cv2.inRange(hsv,blue_lower,blue_upper)
+    jaune=cv2.inRange(hsv,yellow_lower,yellow_upper)
+    
+    #Transformation morphologique, Dilatation    
+    kernal = np.ones((5 ,5), "uint8")
 
-    # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    rouge=cv2.dilate(rouge, kernal)
+    res=cv2.bitwise_and(img, img, mask = rouge)
 
-    # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(frame,frame, mask= mask)
+    bleu=cv2.dilate(bleu,kernal)
+    res1=cv2.bitwise_and(img, img, mask = bleu)
 
-    cv2.imshow('frame',frame)
-    cv2.imshow('mask',mask)
-    cv2.imshow('res',res)
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:
-        break
+    jaune=cv2.dilate(jaune,kernal)
+    res2=cv2.bitwise_and(img, img, mask = jaune)    
 
-cv2.destroyAllWindows()
+
+    #Suivi de la couleur rouge
+    (_,contours,hierarchy)=cv2.findContours(rouge,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if(area>300):
+            
+            x,y,w,h = cv2.boundingRect(contour) 
+            img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+            cv2.putText(img,"Rouge",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255))
+            
+    #Suivi de la couleur bleu
+    (_,contours,hierarchy)=cv2.findContours(bleu,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if(area>300):
+            x,y,w,h = cv2.boundingRect(contour) 
+            img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            cv2.putText(img,"Bleu",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,0))
+
+    #Suivi de la couleur jaune
+    (_,contours,hierarchy)=cv2.findContours(jaune,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if(area>300):
+            x,y,w,h = cv2.boundingRect(contour) 
+            img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+            cv2.putText(img,"jaune",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,255,0))  
+            
+
+        cv2.imshow("Color Tracking",img)
+  
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            cap.release()
+            cv2.destroyAllWindows()
+            break  
+          
+
+    
